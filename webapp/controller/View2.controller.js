57dd66a1643646f16d246f88b09a78089f6497a6
Shruti Kumari <shrutikumari0811@gmail.com>
2023-10-26 12:09:06 +0000

    screen done

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -9,7 +9,28 @@ sap.ui.define([
  
         return Controller.extend("nauticalfe.controller.View2", {
             onInit: function () {
+                var oView = this.getView();
+
+      // Button event handlers
+      oView.byId("_IDGenButton1").attachPress(function () {
+        alert("Create Voyage button clicked!");
+      });
+
+      oView.byId("_IDGenButton2").attachPress(function () {
+        alert("Freight Simulator button clicked!");
+      });
+
+      oView.byId("_IDGenButton3").attachPress(function () {
+        alert("Calculate button clicked!");
+      });
+
+      oView.byId("_IDGenButton4").attachPress(function () {
+        alert("Refresh button clicked!");
+      });
+    }
+  });
+});
  
-            }
-        });
-    });
+            
+        
+    
