ab145fb8503018e0984109d1f01443ff09c487ce
Shruti Kumari <shrutikumari0811@gmail.com>
2023-11-23 11:39:09 +0000

    voyage option and change voyage screen created

---
+++ webapp/controller/TrVoyageOptions.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+ 
+        return Controller.extend("nauticalfe.controller.TrVoyageOptions", {
+            onInit: function () {
+                var voyage = this.getView().byId("VoyageOperations")
+                voyage.addStyleClass("VoyageOperations")
+
+                var heading = this.getView().byId("HeadOptions")
+                heading.addStyleClass("HeadOptions")
+
+                // var alloptions = this.getView().byId("alloptions") //for more dropdown
+                // alloptions.addStyleClass("alloptions")
+            },
+            CreateVoyage:function(){
+                const oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("CreateVoyage")    
+            }
+        });
+    });
--- webapp/controller/TransView.controller.js
+++ webapp/controller/TransView.controller.js
@@ -7,7 +7,16 @@ sap.ui.define(
   
       return BaseController.extend("nauticalfe.controller.TransView", {
         onInit() {
-        }
+        },
+        onChange: function() {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("RouteTrChangeVoyage");
+      },
+      VoyageOptions:function(){
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter.navTo("TrVoyageOptions")
+}
+
       });
     }
   );
