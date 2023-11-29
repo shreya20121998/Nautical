fc9dfbd367bb9475beea2a82db8e7976705c217c
Shruti Kumari <shrutikumari0811@gmail.com>
2023-11-29 12:30:54 +0000

    long text added

--- webapp/controller/TrCreateVoyagePlan.controller.js
+++ webapp/controller/TrCreateVoyagePlan.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    'sap/m/MessageToast'
+    'sap/m/MessageToast',
+    "sap/ui/model/json/JSONModel",
+	"sap/ui/webc/main/Toast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -10,9 +12,10 @@ sap.ui.define([
  
         return Controller.extend("nauticalfe.controller.TrCreateVoyagePlan", {
             onInit: function () {
-              
+               
                
             },
+            
     
             handleNav: function(evt) {
                 var navCon = this.byId("navCon");
@@ -24,6 +27,29 @@ sap.ui.define([
                     navCon.back();
                 }
             }
+            // handleUploadComplete: function(oEvent) {
+            //     var sResponse = oEvent.getParameter("response"),
+            //         aRegexResult = /\d{4}/.exec(sResponse),
+            //         iHttpStatusCode = aRegexResult && parseInt(aRegexResult[0]),
+            //         sMessage;
+    
+            //     if (sResponse) {
+            //         sMessage = iHttpStatusCode === 200 ? sResponse + " (Upload Success)" : sResponse + " (Upload Error)";
+            //         MessageToast.show(sMessage);
+            //     }
+            // },
+    
+            // handleUploadPress: function() {
+            //     var oFileUploader = this.byId("fileUploader");
+            //     oFileUploader.checkFileReadable().then(function() {
+            //         oFileUploader.upload();
+            //     }, function(error) {
+            //         MessageToast.show("The file cannot be read. It may have changed.");
+            //     }).then(function() {
+            //         oFileUploader.clear();
+            //     });
+            // }
+            
 		
     });
 })
